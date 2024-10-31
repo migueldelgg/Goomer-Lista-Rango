25307f303f117713def0dc187a258668f5c40f92
Miguel <delgdevoleper@gmail.com>
2024-10-31 20:37:39 -0300

    Projeto criado, mãos à obra

---
+++ src/main/java/migueldelgg/com/github/demo/GoomerListaRangoApplication.java
@@ -0,0 +1,13 @@
+package migueldelgg.com.github.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoomerListaRangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoomerListaRangoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/migueldelgg/com/github/demo/GoomerListaRangoApplicationTests.java
@@ -0,0 +1,13 @@
+package migueldelgg.com.github.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoomerListaRangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
